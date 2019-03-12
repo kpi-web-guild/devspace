54836d19ac1b7809f315629df1cab001c161d8ed
RootenberG <savichevdenis244@gmail.com>
2019-03-13 00:45:09 +0200

    Updates: programs are installed by one command, automation of answers Yes or no, appearance is improved.

--- aut-apps.sh
+++ aut-apps.sh
@@ -1,13 +1,18 @@
-#!/usr/bin/env bash
-echo 'Instal Googe Chrome'
-sudo bash -c "touch /etc/apt/sources.list.d/google-chrome.list && echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main'>/$
+#! /usr/bin/env bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+echo 'Install Google Chrome, Git and Visual Studio Code'
+touch /etc/apt/sources.list.d/google-chrome.list
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main'>/etc/apt/sources.list.d/google-chrome.list
 wget https://dl.google.com/linux/linux_signing_key.pub
-sudo bash -c "apt-key add linux_signing_key.pub && apt update && apt install google-chrome-stable"
-echo 'Chrome installed, installing git..'
-sudo apt-get install git
-echo 'Git installed, installing Visual studio code'
+apt-key add linux_signing_key.pub
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo bash -c "install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/"
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo bash -c "apt-get install apt-transport-https && apt-get update && apt-get install code"
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt update
+echo 'Start install'
+apt-get -y install google-chrome-stable git apt-transport-https code
+apt update
 echo 'Complete'
