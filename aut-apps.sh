66d0634476ffa20787b6310cc7cdb7536c539a63
RootenberG <savichevdenis244@gmail.com>
2019-03-13 21:47:26 +0200

    update.

--- aut-apps.sh
+++ aut-apps.sh
@@ -4,11 +4,11 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 touch /etc/apt/sources.list.d/google-chrome.list
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main'>/etc/apt/sources.list.d/google-chrome.list
+echo 'deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main'>/etc/apt/sources.list.d/google-chrome.list
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add linux_signing_key.pub
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 apt update
 echo 'Start install'
 apt-get -y install google-chrome-stable git apt-transport-https code
