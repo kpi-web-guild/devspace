91f5405af9c9b6c57b14b036769098bb0df5ccfd
RootenberG <savichevdenis244@gmail.com>
2019-03-13 23:27:28 +0200

    Update.

--- aut-apps.sh
+++ aut-apps.sh
@@ -7,7 +7,7 @@ touch /etc/apt/sources.list.d/google-chrome.list
 echo 'deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main'>/etc/apt/sources.list.d/google-chrome.list
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add linux_signing_key.pub
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 apt update
 echo 'Start install'
