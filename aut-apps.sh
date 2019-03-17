19794fe48ed940f14ad79a762297473f587de248
RootenberG <savichevdenis244@gmail.com>
2019-03-17 18:41:39 +0200

    Improved code appearance and use temporary directoty to add a key

--- aut-apps.sh
+++ aut-apps.sh
@@ -3,13 +3,15 @@ if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-touch /etc/apt/sources.list.d/google-chrome.list
+tmp_dir=$(mktemp -d -t pub_key-XXXXXX)
+echo $tmp_dir
 echo 'deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main'>/etc/apt/sources.list.d/google-chrome.list
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add linux_signing_key.pub
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > $tmp_dir/microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 apt update
 echo 'Start install'
 apt-get -y install google-chrome-stable git apt-transport-https code
+rm -rf $tmp_dir
 echo 'Complete'
